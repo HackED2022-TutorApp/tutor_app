7b7beeaaa68c3cd3a58957a601a96127d54d9b61
Brad Conklin <bconklin@ualberta.ca>
2022-01-15 16:21:26 -0700

    remove accounts dir

--- uatutor/uatutor/urls.py
+++ uatutor/uatutor/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('accounts/', include('accounts.urls'))
     path('', include('api.urls'))
 ]
