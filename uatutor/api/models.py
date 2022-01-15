afa10d95351ef9d2b7a6003401b3d422d5a5dfb7
Xuanhao Wang <67352673+ccBrita@users.noreply.github.com>
2022-01-15 15:46:45 -0700

    Models

--- uatutor/api/models.py
+++ uatutor/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Tutor(models.Model):
+    FirstName = models.CharField(max_length=30)
+    LastName = models.CharField(max_length=20)
+    dob = models.DateField()
+    description = models.TextField(max_length=150)
+    degreeChoices =[
+        ('Ba/BSc.', "Bachelors"),
+        ('MSc', "Master"),
+        ('Phd', 'Doctoral')
+    ]
+    degree = models.CharField(choices=degreeChoices, max_length=20)
+    major = models.CharField(max_length=20, blank=False, null=False)
+    minor = models.CharField(max_length=20, blank =True, null=True)
+    subject = models.CharField(max_length=20)
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    date_of_birth = models.DateField()
