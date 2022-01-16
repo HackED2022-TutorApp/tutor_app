d418263f6d4b1f028f7601f42ed559e276d79b8e
Glenn Storch <storch@ualberta.ca>
2022-01-16 02:19:30 -0700

    Profile and EditProfile now grab hardcoded info from database

--- uatutor/frontend/src/EditProfile.js
+++ uatutor/frontend/src/EditProfile.js
@@ -1,7 +1,11 @@
 import {Link} from 'react-router-dom'
+import useFetch from "./useFetch";
 
 const EditProfile = () => {
     
+    /*const { id } = useParams();*/
+    const {data : tutor, isPending, error} = useFetch('http://localhost:8000/api/tutor/' + 1 + '/');
+
     return ( 
         <div className="editprofile">
             <h2>Your Profile - Editing</h2>
@@ -10,43 +14,49 @@ const EditProfile = () => {
                     <h3>Name:</h3>
                         <input 
                             type="text"
-                            defaultValue="test"
-                        />
+                            defaultValue={tutor && tutor.FirstName}
+                            required
+                            />
                 </div>
                 <div className='editprofile'>
                     <h3>Degree:</h3>
                         <select>
-                            
+                            {tutor && <option value={tutor.degree}>{tutor.degree}</option>}
                         </select>
                 </div>
                 <div className='editprofile'>
                     <h3>Major:</h3>
-                        <input 
-                            type="text"
-                            required
-                        />
+                        <select>
+                        {tutor && <option value={tutor.major}>{tutor.major}</option>}
+                        </select>
                 </div>
                 <div className='editprofile'>
                     <h3>Minor:</h3>
                         <input 
                             type="text"
+                            defaultValue={tutor && tutor.minor}
                             required
-                        />
+                            />
                 </div>
                 <div className='editprofile'>
                     <h3>Subjects of Interest:</h3>
                         <input 
                             type="text"
+                            defaultValue={tutor && tutor.subject}
                             required
-                        />
+                            />
                 </div>
             </form>
 
             <div className="links">
                 <Link to="/profile">Save</Link>
             </div>
+
+            {error && <div>{error}</div> }
+            {isPending && <div>Loading...</div>}
+
         </div>
      );
-}
- 
-export default EditProfile;
+    }
+    
+    export default EditProfile;
--- uatutor/frontend/src/Profile.js
+++ uatutor/frontend/src/Profile.js
@@ -1,20 +1,27 @@
 import {Link} from 'react-router-dom'
+import useFetch from "./useFetch";
 
 const Profile = () => {
     
+    const {data : tutor, isPending, error} = useFetch('http://localhost:8000/api/tutor/' + 1 + '/');
+
     return ( 
         <div className="profile">
             <h2>Your Profile</h2>
-            <h3>Name:</h3>
-            <h3>Degree:</h3>
-            <h3>Major:</h3>
-            <h3>Minor:</h3>
-            <h3>Subjects of Interest:</h3>
+            <h3>Name: {tutor && tutor.FirstName}</h3>
+            <h3>Degree: {tutor && tutor.degree}</h3>
+            <h3>Major: {tutor && tutor.major}</h3>
+            <h3>Minor: {tutor && tutor.minor}</h3>
+            <h3>Subjects of Interest: {tutor && tutor.subject}</h3>
 
 
             <div className="links">
                 <Link to="/editprofile">Edit Profile</Link>
             </div>
+
+            {error && <div>{error}</div> }
+            {isPending && <div>Loading...</div>}
+
         </div>
      );
 }
