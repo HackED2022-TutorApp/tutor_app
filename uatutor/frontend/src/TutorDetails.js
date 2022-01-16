d623c05bf4516c3608d1e5313942c5abe9644cd7
Xuanhao Wang <67352673+ccBrita@users.noreply.github.com>
2022-01-16 01:18:39 -0700

    link list to detail page

--- uatutor/frontend/src/TutorDetails.js
+++ uatutor/frontend/src/TutorDetails.js
@@ -6,11 +6,18 @@ const TutorDetails = () => {
     const {data : tutor, isPending, error} = useFetch('http://localhost:8000/api/tutor/' + id + '/');
 
     return (
-        <div className="blog-details">
+        <div className="tutor-details">
             {error && <div>{error}</div> }
             {isPending && <div>Loading...</div>}
-            {tutor && <h2>Tutor Details</h2>}
+            {tutor && <h1>Tutor Details</h1>}
             {tutor && <h2>{ tutor.FirstName }</h2> }
+            {tutor && <p class='degree'>Degree: { tutor.degree }</p> }
+            {tutor && <p class='major'>Major: { tutor.major }</p> }
+            {tutor && tutor.minor && <p class="minor">Minor: { tutor.minor }</p> }
+        
+            {tutor && <p class="description">Courses: { tutor.subject }</p> } 
+            {tutor && <p class="description">{ tutor.description }</p> }
+
         </div>
     );
 }
