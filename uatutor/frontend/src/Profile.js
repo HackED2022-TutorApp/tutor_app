16e482894889fa020f0e5724f81ca6fd543d3cc2
Harish <saurab@ualberta.ca>
2022-01-15 18:06:15 -0700

    added test_profile

--- uatutor/frontend/src/Profile.js
+++ uatutor/frontend/src/Profile.js
@@ -1,7 +1,17 @@
+
 const Profile = () => {
+    
     return ( 
         <div className="profile">
             <h2>Your Profile</h2>
+            <h3>Name:</h3>
+            <h3>Degree:</h3>
+            <h3>Major:</h3>
+            <h3>Minor:</h3>
+            <h3>Subjects of Interest:</h3>
+
+
+
         </div>
      );
 }
